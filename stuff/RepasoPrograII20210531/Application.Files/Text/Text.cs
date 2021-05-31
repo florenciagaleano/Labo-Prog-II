02bc039db9e2bbbb72d4d7798c0b2fb0ee263f86
florenciagaleano <florgal2001@gmail.com>
2021-05-31 20:29:03 -0300

    Repaso

---
+++ stuff/RepasoPrograII20210531/Application.Files/Text/Text.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Files.Text
+{
+    public class Text<T> : IFile<T>
+    {
+
+        public bool Save(string file, T data)
+        {
+            bool retorno = false;
+            try
+            {
+                using (StreamWriter w = new StreamWriter(file))
+                {
+                    w.WriteLine(data.ToString());
+                }
+                retorno = true;
+            }catch(Exception e)
+            {
+
+            }
+
+            return retorno;
+        }
+
+        public bool Read(string file, out T data)
+        {
+            //bool retorno = false;
+            //try
+            //{
+            //    using(StreamReader reader = new StreamReader(file))
+            //    {
+            //       data = default;
+            //    }
+            //    retorno = true;
+            //}catch(Exception e)
+            //{
+
+            //}
+
+            data = default;
+            return true;
+        }
+    }
+}
---
+++ stuff/RepasoPrograII20210531/Application.Files/Xml/Xml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using Application.Models;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Application.Files.Xml
+{
+    public class Xml<T> : IFile<T>
+    {
+        //TODO implementar IFile
+        public bool Read(string path, out List<Application.Models.Customer> customers)
+        {
+            List<Application.Models.Customer> lista = null;
+            bool ret = false;
+            try
+            {
+                using(XmlTextReader r = new XmlTextReader(path))
+                {
+                    XmlSerializer serializar = new XmlSerializer(typeof(List<Application.Models.Customer>));
+                    lista = (List<Application.Models.Customer>)serializar.Deserialize(r);
+                }
+                ret = true;
+            }catch(Exception)
+            {
+
+            }
+
+            customers = lista;
+            return ret;
+        }
+
+        public bool Read(string file, out T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(string file, T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
