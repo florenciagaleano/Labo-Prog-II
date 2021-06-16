f1d0ef5ba344a286519beb90da1d00df65c2c764
florenciagaleano <florgal2001@gmail.com>
2021-06-15 22:11:38 -0300

    ej en clase

---
+++ stuff/Hilos+Eventos Cajero/Entidades/Cajero.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public delegate bool DelegadoCajero(Persona persona);
+    public class Cajero
+    {
+        private List<Thread> hiloCajas;
+        public static Queue<Persona> personas;
+
+        public Cajero()
+        {
+            this.hiloCajas = new List<Thread>();
+            personas = new Queue<Persona>();
+        }
+
+        public void Cobrar(Persona p, Cajero c)
+        {
+            float montoTotal = 0;
+            foreach (Factura item in p.Factura)
+            {
+                montoTotal += item.Precio;
+            }
+            p.MontoTotal = montoTotal;
+            Thread nuevoHilo = new Thread(p.MockClicloCajero);
+            c.hiloCajas.Add(nuevoHilo);
+
+            nuevoHilo.Start();
+        }
+
+        public void CerarCajero()
+        {
+            foreach (Thread item in this.hiloCajas)
+            {
+                if (item.IsAlive)
+                    item.Abort();
+            }
+        }
+    }
+}
---
+++ stuff/ThreadsYT/ThreadsYT/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(MetodoSaludo);
+            Thread t2 = new Thread(MetodoSaludo);
+
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine("Hola desde thread 1!!");
+            Thread.Sleep(500);
+            Console.WriteLine("Hola desde thread 1!!");
+            Console.WriteLine("Hola desde thread 1!!");
+            Console.WriteLine("Hola desde thread 1!!");
+            Console.WriteLine("Hola desde thread 1!!");
+
+           // MetodoSaludo();
+        }
+
+        static void MetodoSaludo()
+        {
+            Console.WriteLine("Hola desde thread 2!!");
+            Thread.Sleep(500);
+            Console.WriteLine("Hola desde thread 2!!");
+            Console.WriteLine("Hola desde thread 2!!");
+            Console.WriteLine("Hola desde thread 2!!");
+            Console.WriteLine("Hola desde thread 2!!");
+
+            Console.ReadKey();
+        }
+    }
+}
