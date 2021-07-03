956ec76ef3cd8971e7bbfbaf1e5db8fb3ca13158
florenciagaleano <florgal2001@gmail.com>
2021-07-02 23:47:20 -0300

    parcial votacion hecho sql andando omg

---
+++ stuff/2do Parcial vamos q se puede !!!/Parcial Votacion/20180628-SP - Alumno/Entidades/DAO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Entidades
+{
+    public class DAO : IArchivos<Votacion>
+    {
+        public bool Guardar(string rutaArchivo, Votacion objeto)
+        {
+            bool ret = false;
+            SqlConnection conexion = null;
+
+            try
+            {
+                conexion = new SqlConnection(rutaArchivo);
+
+                conexion.Open();
+                //SqlCommand comando = new SqlCommand("INSERT INTO Votaciones(nombreLey,afirmativos,negativos,abstenciones,nombreAlumno) VALUES('{0}',{1},{2},{3},'florrrr')", conexion);
+
+                SqlCommand comando = new SqlCommand();
+                comando.Connection = conexion;
+                comando.CommandType = System.Data.CommandType.Text;
+
+                comando.CommandText = "INSERT INTO Votaciones (nombreLey,afirmativos,negativos,abstenciones,nombreAlumno) VALUES (@nombreLey,@afirmativos,@negativos,@abstenciones,@nombreAlumno)";
+                comando.Parameters.AddWithValue("@nombreLey",objeto.NombreLey);
+                comando.Parameters.AddWithValue("@afirmativos",objeto.ContadorAfirmativo.ToString());
+                comando.Parameters.AddWithValue("@negativos", objeto.ContadorNegativo.ToString());
+                comando.Parameters.AddWithValue("@abstenciones",objeto.ContadorAbstencion.ToString());
+                comando.Parameters.AddWithValue("@nombreAlumno","flor");
+
+
+                comando.ExecuteNonQuery();
+                ret = true;
+            }catch(Exception e)
+            {
+                throw new ErrorArchivoException(e);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+
+            return ret;
+        }
+
+        public Votacion Leer(string rutaArchivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ stuff/2do Parcial vamos q se puede !!!/Parcial Votacion/20180628-SP - Alumno/Entidades/SerializarXML.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Entidades
+{
+    public class SerializarXML<T> : IArchivos<T>
+    {
+        public bool Guardar(string rutaArchivo, T objeto)
+        {
+            try
+            {
+                XmlTextWriter writer = new XmlTextWriter(rutaArchivo, Encoding.UTF8);
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                ser.Serialize(writer, objeto);
+                writer.Close();
+                return true;
+            }
+            catch (Exception innerException)
+            {
+                throw new ErrorArchivoException(innerException);
+            }
+        }
+
+        public T Leer(string rutaArchivo)
+        {
+            Votacion ret = default;
+            try
+            {
+                using (XmlTextReader xmlWriter = new XmlTextReader(rutaArchivo))
+                {
+                    XmlSerializer serializar = new XmlSerializer(typeof(Votacion));
+
+                    return (T)serializar.Deserialize(xmlWriter);
+                }
+                
+            }
+            catch (Exception e) {
+
+                throw new ErrorArchivoException(e);
+
+            }
+        }
+    }
+}
